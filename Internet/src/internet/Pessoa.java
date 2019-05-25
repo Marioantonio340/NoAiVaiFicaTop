7634b057c54f7ecf80b496cddc0b8707d53e1df7
Marioantonio340 <marioantonio340@gmail.com>
2019-05-25 16:36:10 -0300

    implementação classe abstrata Pessoa

--- Internet/src/internet/Pessoa.java
+++ Internet/src/internet/Pessoa.java
@@ -1,5 +1,19 @@
 package internet;
 
 public abstract class Pessoa {
-
+	protected String nome;
+	protected int cpf;
+	
+	
+	public abstract String getNome();
+	
+	public abstract void setNome(String nome);
+	
+	public abstract int getCpf();
+	
+	public abstract void setCpf(int cpf);
+	
+	
+	
+	
 }
